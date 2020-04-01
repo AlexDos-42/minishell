cb6600416d16488d8c4c99495a1fb9abe3923155
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2020-04-01 13:07:20 +0200

    Create ft_echo.c

---
+++ srcs/ft_echo.c
@@ -0,0 +1,26 @@
+# include "../include/minishell.h"
+
+void	ft_echo(t_all *all)
+{
+	int j;
+	char *tmp;
+
+	j = ft_strlen(all->tab);
+	if (!ft_strncmp(all->tab, "-n", 2) && all->tab[2] == ' ')
+	{
+		if (all->tab[j - 1] == '\n')
+			all->tab[j - 1] = '\0';
+		tmp = ft_strtrim(all->tab + 2, " ");
+		ft_putstr_fd(tmp, 1);
+		free(tmp);
+	}
+	else
+	{
+		tmp = ft_strtrim(all->tab, " ");
+		ft_putstr_fd(tmp, 1);
+		if (all->tab[j - 1] != '\n')
+			write(1, "\n", 1);
+		free(tmp);
+	}
+	free(all->tab);
+}
