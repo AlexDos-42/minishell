26085bdb2ddc0393bfb6e1eefb9e20d6c55f8312
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2020-04-01 13:09:03 +0200

    Create ft_cd.c

---
+++ srcs/ft_cd.c
@@ -0,0 +1,24 @@
+# include "../include/minishell.h"
+
+void	ft_cd(t_all *all)
+{
+	int j;
+	char *tmp;
+
+	j = ft_strlen(all->tab);
+	if (all->tab[j - 1] == '\n')
+		all->tab[j - 1] = '\0';
+	
+	tmp = ft_strtrim(all->tab, " ");
+	free(all->tab);
+	all->tab = tmp;
+	if (all->tab[0] == '~')
+	{
+		chdir("/home");
+		all->pwd = getcwd(all->pwd, 510);
+	}
+	else if (chdir(all->tab) == 0)
+		all->pwd = getcwd(all->pwd, 510);
+	else
+		printf("nope\n");
+}
