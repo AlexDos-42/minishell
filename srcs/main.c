7b4518b9672803e2f58b18ea43db5e106112949a
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2020-04-01 12:54:04 +0200

    Update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -73,65 +73,6 @@ void	ft_echo(t_all *all)
 	free(all->tab);
 }
 
-void	ft_ptrfct(t_all *all)
-{
-	void		(*fonc[7])(t_all *);
-
-	if (all->fct != 0)
-	{
-		fonc[1] = ft_echo;
-		fonc[2] = ft_pwd;
-		fonc[3] = ft_cd;
-		fonc[4] = ft_exit;
-		fonc[5] = ft_env;
-		fonc[all->fct](all);
-		all->fct = 0;
-	}
-}
-
-void	ft_nbfct(t_all *all, char *tab)
-{
-	int i;
-
-	i = 0;
-	while (tab[i] != ' ' && tab[i] != '\n')
-		i++;
-	if (!ft_strncmp(tab, "echo", 4) && tab[4] == ' ')
-		all->fct = 1;
-	else if (!ft_strncmp(tab, "pwd", 3))
-		all->fct = 2;
-	else if (!ft_strncmp(tab, "cd", 2))
-		all->fct = 3;
-	else if (!ft_strncmp(tab, "exit", 4))
-		all->fct = 4;
-	else if (!ft_strncmp(tab, "env", 3))
-		all->fct = 5;
-	while (tab[i] == ' ')
-		i++;
-	all->tab = ft_substr(tab, i, ft_strlen(tab) - i);
-}
-
-void	ft_minishell(t_all *all, char *str)
-{
-	int i;
-	int k;
-	char **tab;
-	
-	tab = ft_split(str, ';');
-	k = 0;
-	while (tab[k])
-	{
-		i = 0;
-		while (tab[k][i] == ' ')
-			i++;
-		ft_nbfct(all, &tab[k][i]);
-		free(tab[k]);
-		ft_ptrfct(all);
-		k++;
-	}
-	free(tab);
-}
-
 void	ft_prompt(t_all *all, char *tmp, char *str)
 {
 	int		i;
