33e6bdb70856e174f2300414f5f519730ef679d7
Alex 42 <92.alex.ds@gmail.com>
2020-09-07 20:11:22 +0200

    Add files via upload

--- srcs/ft_exec.c
+++ srcs/ft_exec.c
@@ -2,8 +2,8 @@
 
 char		*isexec(char *tab)
 {
-	int		i;
-	int		j;
+	int				i;
+	int				j;
 
 	i = 0;
 	j = ft_strlen(tab);
@@ -16,80 +16,76 @@ char		*isexec(char *tab)
 	}
 	while (j != 0 && tab[j - 1] == ' ')
 		j--;
-    tab = ft_substr(tab, i, j - i);
-    return (tab);
+	tab = ft_substr(tab, i, j - i);
+	return (tab);
 }
 
-char **ft_allpath(t_all *all)
+char		**ft_allpath(t_all *all)
 {
-    char *tmp;
-    char **path;
-    int     i;
+	char			*tmp;
+	char			**path;
+	int				i;
 
-    i = 0;
-    path = NULL;
-    while (all->env[i])
-    {
-        if (!ft_strncmp(all->env[i], "PATH", 4))
-        {
-            tmp = ft_strdup(&all->env[i][5]);
-            path = ft_split(tmp, ':');
-            free(tmp);
-            break;
-        }
-        i++;
-    }
-    return (path);
+	i = 0;
+	path = NULL;
+	while (all->env[i])
+	{
+		if (!ft_strncmp(all->env[i], "PATH", 4))
+		{
+			tmp = ft_strdup(&all->env[i][5]);
+			path = ft_split(tmp, ':');
+			free(tmp);
+			break ;
+		}
+		i++;
+	}
+	return (path);
 }
 
-void ft_free(char **tab)
+void		ft_free(char **tab)
 {
-    int i;
+	int				i;
 
-    i = -1;
-    while(tab[++i])
-        free(tab[i]);
-    free(tab);
+	i = -1;
+	while (tab[++i])
+		free(tab[i]);
+	free(tab);
 }
 
-char *ft_exist(t_all *all, char *tab)
+char		*ft_exist(t_all *all, char *tab, int i)
 {
-    DIR             *dir;
-    struct dirent    *dp;
-    char **path;
-    int i;
+	DIR				*dir;
+	struct dirent	*dp;
+	char			**path;
+	char			*tmp;
 
-    i = 0;
-    if ((path = ft_allpath(all)) != NULL)
-    {
-        while(path[i])
-        {
-            dir = opendir(path[i]);
-            while ((dp = readdir(dir)) != NULL)
-            {
-                if (!ft_strncmp(tab, dp->d_name, ft_strlen(tab))
-                && ft_strlen(tab) == ft_strlen(dp->d_name))
-                {
-                    char *tmp = ft_strjoin(path[i], "/", 0);
-                    tab = ft_strjoin(tmp, tab, 3);
-                    ft_free(path);
-                    closedir(dir);
-                    return(tab);
-                }
-            }
-            closedir(dir);
-            i++;
-        }
-        ft_free(path);
-    }
-    return(tab);
+	if ((path = ft_allpath(all)) != NULL)
+	{
+		while (path[++i])
+		{
+			dir = opendir(path[i]);
+			while ((dp = readdir(dir)) != NULL)
+				if (!ft_strncmp(tab, dp->d_name, ft_strlen(tab))
+				&& ft_strlen(tab) == ft_strlen(dp->d_name))
+				{
+					tmp = ft_strjoin(path[i], "/", 0);
+					tab = ft_strjoin(tmp, tab, 3);
+					ft_free(path);
+					closedir(dir);
+					return (tab);
+				}
+			closedir(dir);
+		}
+		ft_free(path);
+	}
+	return (tab);
 }
 
 int			ft_exec(t_all *all, char *tab)
 {
-	pid_t	pid;
-	int		status;
-    char    **arg;
+	pid_t			pid;
+	int				status;
+	char			**arg;
 
 	status = 0;
 	pid = fork();
@@ -98,17 +94,17 @@ int			ft_exec(t_all *all, char *tab)
 		if (ft_strlen(tab) && tab[ft_strlen(tab) - 1] == '\n')
 			tab[ft_strlen(tab) - 1] = '\0';
 		tab = isexec(tab);
-        arg = ft_split(tab, ' ');
-        if (!arg[0])
-            exit(0);
-        arg[0] = ft_exist(all, arg[0]);
-        if (execve(arg[0], arg, all->env) == -1)
+		arg = ft_split(tab, ' ');
+		if (!arg[0])
+			exit(0);
+		arg[0] = ft_exist(all, arg[0], -1);
+		if (execve(arg[0], arg, all->env) == -1)
 		{
 			ft_printf("error %s\n", strerror(errno));
 			exit(127);
 		}
-    }
-    wait(&status);
-    ret = WEXITSTATUS(status);    
+	}
+	wait(&status);
+	ret = WEXITSTATUS(status);
 	return (0);
 }
