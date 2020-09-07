706f50213eb069c7f4eba21dc0ced0917e9d5ace
Alex 42 <92.alex.ds@gmail.com>
2020-09-07 16:12:59 +0200

    Add files via upload

--- srcs/ft_exec.c
+++ srcs/ft_exec.c
@@ -20,13 +20,59 @@ char		*isexec(char *tab)
 	return (tab);
 }
 
+char **ft_allpath(t_all *all)
+{
+    char *tmp;
+    char **path;
+    int     i;
 
+    i = 0;
+    while (all->env[i])
+    {
+        if (!ft_strncmp(all->env[i], "PATH", 4))
+        {
+            tmp = ft_strdup(&all->env[i][5]);
+            path = ft_split(tmp, ':');
+            break;
+        }
+        i++;
+    }
+    return (path);
+}
+
+char *ft_exist(t_all *all, char *tab)
+{
+    DIR             *dir;
+    struct dirent    *dp;
+    char **path;
+    int i;
+
+    i = 0;
+    path = ft_allpath(all);
+    while(path[i])
+    {
+        dir = opendir(path[i]);
+        while ((dp = readdir(dir)) != NULL)
+        {
+            if (!ft_strncmp(tab, dp->d_name, ft_strlen(tab))
+            && ft_strlen(tab) == ft_strlen(dp->d_name))
+            {
+                char *tmp = ft_strjoin(path[i], "/", 1);
+                tab = ft_strjoin(tmp, tab, 12);
+                closedir(dir);
+                return(tab);
+            }
+        }
+        closedir(dir);
+        i++;
+    }
+    return(tab);
+}
 
 int			ft_exec(t_all *all, char *tab)
 {
 	pid_t	pid;
 	int		status;
-	//pid_t	wpid;
     char    **arg;
 
 	status = 0;
@@ -36,8 +82,9 @@ int			ft_exec(t_all *all, char *tab)
 		if (ft_strlen(tab) && tab[ft_strlen(tab) - 1] == '\n')
 			tab[ft_strlen(tab) - 1] = '\0';
 		tab = isexec(tab);
-        arg = ft_split(tab, ' '); 
-		if (execve(arg[0], arg, all->env) == -1)
+        arg = ft_split(tab, ' ');
+        arg[0] = ft_exist(all, arg[0]);
+        if (execve(arg[0], arg, all->env) == -1)
 		{
             ret = 127;
 			ft_printf("error %s\n", strerror(errno));
@@ -46,7 +93,7 @@ int			ft_exec(t_all *all, char *tab)
 	}
 	else
         wait(&status);
-    ret = getpid();
-    ft_printf("ret %d\n", ret);
+    ret = status;
+    //ft_printf("ret %d\n", ret);
 	return (0);
 }
