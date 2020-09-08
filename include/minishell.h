32d26e8fb95a02855ae298e5ea24a2b1c906a69c
Alex 42 <92.alex.ds@gmail.com>
2020-09-08 12:03:24 +0200

    Add files via upload

--- include/minishell.h
+++ include/minishell.h
@@ -8,6 +8,7 @@
 # include <sys/errno.h>
 # include <sys/types.h>
 # include <sys/wait.h>
+# include <dirent.h>
 # include "error.h"
 
 int			inter;
@@ -82,7 +83,7 @@ int				ft_ispipe(char *tab);
 */
 
 int 			ft_redirection(char *tab, t_all *all);
-int				ft_create_file(char *tab, int *fd, char *file, int *i);
+int				ft_create_file(char *tab);
 
 
 /*
