55d6e7124b78c91d9696a5b8cafe01de37528c5e
Alex 42 <92.alex.ds@gmail.com>
2020-09-09 14:49:53 +0200

    Add files via upload

--- srcs/ft_unset.c
+++ srcs/ft_unset.c
@@ -12,67 +12,49 @@
 
 #include "../include/minishell.h"
 
-int		iserror;
-char	*messerror;
-
-void	ft_messerror(t_all *all, int j, int i)
+void			ft_messerror(t_all *all, int j, int i)
 {
-	if (iserror == 0)
-	{
-		messerror = ft_substr(all->tab, j, i);
-		messerror = ft_strjoin("unset: ", messerror, 2);
-		messerror = ft_strjoin(messerror, ": invalid parameter name\n", 1);
-	}
-	else
-	{
-		messerror = ft_substr(all->tab, j, i);
-		messerror = ft_strjoin("zsh: ", messerror, 2);
-		messerror = ft_strjoin(messerror, " not found\n", 1);
-	}
+	char			*messerror;
+
+	messerror = ft_substr(all->tab, j, i);
+	messerror = ft_strjoin("minishell: unset: « ", messerror, 2);
+	messerror = ft_strjoin(messerror, " » : invalid parameter name", 1);
+	ft_printf("%s\n", messerror);
+	free(messerror);
 }
 
-void	ft_error(t_all *all, int j)
+int				ft_error(t_all *all, int j)
 {
-	int i;
-	int k;
+	int				i;
+	int				k;
 
 	k = 0;
 	i = 0;
+	ret = 1;
 	while (all->tab[j + i] != '=')
 		i++;
 	if (!all->tab[j + i + 1] || all->tab[j + i + 1] == ' ')
+		ft_messerror(all, j, i + 1);
+	else
 	{
-		if (iserror == 0)
-		{
-			ft_messerror(all, j, i + 1);
-			iserror = 1;
-		}
-	}
-	else if (iserror != 2)
-	{
-		if (iserror == 1)
-			free(messerror);
-		iserror = 2;
+		ft_printf("test\n");
 		while (all->tab[j + k] && all->tab[j + k + 1] != ' ')
 			k++;
-		ft_messerror(all, j, k);
+		ft_messerror(all, j, k + 1);
 	}
+	return (0);
 }
 
-void	ft_suprenv(t_all *all, int j)
+void			ft_suprenv(t_all *all, int j, unsigned int p, int o)
 {
-	unsigned int	p;
-	int				o;
 	unsigned int	i;
 	unsigned int	k;
 	char			**new_tab;
 
-	p = 0;
 	while (all->env[p] && p < all->nb_env - 1)
 	{
-		o = 0;
-		while (all->env[p][o] && (all->env[p][o] == all->tab[j + o]))
-		{
+		o = -1;
+		while (all->env[p][++o] && (all->env[p][o] == all->tab[j + o]))
 			if (all->env[p][o + 1] == '=')
 			{
 				i = 0;
@@ -82,29 +64,26 @@ void	ft_suprenv(t_all *all, int j)
 				{
 					if (i != p)
 						new_tab[k++] = ft_strdup(all->env[i]);
-					free(all->env[i]);
-					i++;
+					free(all->env[i++]);
 				}
 				free(all->env);
 				all->env = new_tab;
 				return ;
 			}
-			o++;
-		}
 		p++;
 	}
 }
 
-int	ft_unset(t_all *all)
+int				ft_unset(t_all *all)
 {
-	int i;
-	int err;
-	int j;
+	int				i;
+	int				err;
+	int				j;
 
-	iserror = 0;
 	i = -1;
 	err = 0;
 	j = 0;
+	ret = 0;
 	while (all->tab && all->tab[++i])
 	{
 		if (all->tab[i] == '=')
@@ -112,21 +91,12 @@ int	ft_unset(t_all *all)
 		if ((all->tab[i + 1] == ' ' || !all->tab[i + 1]))
 		{
 			if (err == 1)
-			{
-				err = 0;
-				ft_error(all, j);
-			}
+				err = ft_error(all, j);
 			else
-				ft_suprenv(all, j);
+				ft_suprenv(all, j, 0, 0);
 			j = i + 2;
 		}
 	}
 	free(all->tab);
-	if (iserror)
-	{
-		ft_putstr_fd(messerror, 1);
-		free(messerror);
-		return (ret = 1);
-	}
-	return (ret = 0);
+	return (0);
 }
