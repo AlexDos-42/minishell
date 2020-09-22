8af02cf53343ee73832a567948c0af0f8b24f9fa
Emilie <edouvier@student.42.fr>
2020-09-22 13:19:58 +0200

    norme exit

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -85,8 +85,7 @@ int		ft_cleanexit(char *tab)
 				i++;
 			if (new[0][i])
 			{
-				ft_printf("minishell: exit: %s:
-				numeric argument required\n", new[0]);
+				ft_printf("minishell: exit: %s: numeric argument required\n", new[0]);
 				ft_freexec(new);
 				return (2);
 			}
