ee56a00efa863aa147d10816641ccd6a2397edfc
AlexDos-42 <92.alex.ds@gmail.com>
2020-09-24 12:50:41 +0200

    norme

--- include/minishell.h
+++ include/minishell.h
@@ -29,96 +29,96 @@ int			g_ret;
 
 typedef struct		s_all
 {
-	char	**argv;
-	int		fct;
-	char	*tab;
-	char	*pwd;
-	char	**env;
-	int		ret;
-	size_t	nb_env;
-	int		fdin;
-	int		fdout;
-	int		fdinc;
-	int		fdoutc;
+	char			**argv;
+	int				fct;
+	char			*tab;
+	char			*pwd;
+	char			**env;
+	int				ret;
+	size_t			nb_env;
+	int				fdin;
+	int				fdout;
+	int				fdinc;
+	int				fdoutc;
 }					t_all;
 
 /*
 ** --MAIN--
 */
 
-int			main(int argc, char **argv, char **env);
-void		ft_prompt(t_all *all, char *tmp, char *str);
+int					main(int argc, char **argv, char **env);
+void				ft_prompt(t_all *all, char *tmp, char *str);
 
 /*
 ** --PARSING--
 */
 
-int			ft_minishell(t_all *all, char *str);
-void		ft_nbfct(t_all *all, char *tab);
-int			ft_ptrfct(t_all *all);
-int			ft_loop(char *tab, t_all *all);
+int					ft_minishell(t_all *all, char *str);
+void				ft_nbfct(t_all *all, char *tab);
+int					ft_ptrfct(t_all *all);
+int					ft_loop(char *tab, t_all *all);
 
 /*
 ** --INIT--
 */
 
-void		ft_initenv(t_all *all, char **env);
-char		**tab_env(char **env, size_t *nb_env);
+void				ft_initenv(t_all *all, char **env);
+char				**tab_env(char **env, size_t *nb_env);
 
 /*
 ** --FONCTIONS--
 */
 
-int			ft_echo(t_all *all);
-int			ft_pwd(t_all *all);
-int			ft_cd(t_all *all);
-int			ft_exit(t_all *all);
-int			ft_env(t_all *all);
-int			ft_export(t_all *all);
-char		*ft_suprguy(char *tabnewenv);
-int			ft_unset(t_all *all);
+int					ft_echo(t_all *all);
+int					ft_pwd(t_all *all);
+int					ft_cd(t_all *all);
+int					ft_exit(t_all *all);
+int					ft_env(t_all *all);
+int					ft_export(t_all *all);
+char				*ft_suprguy(char *tabnewenv);
+int					ft_unset(t_all *all);
 
 /*
 ** --REPLACE--
 */
 
-char		*ft_replace(char *tab, t_all *all);
-char		*ft_isinenv(char *tab, t_all *all);
-char		*ft_newtab(char *tab, char *env, int i);
+char				*ft_replace(char *tab, t_all *all);
+char				*ft_isinenv(char *tab, t_all *all);
+char				*ft_newtab(char *tab, char *env, int i);
 
 /*
 ** --PIPE--
 */
 
-int			ft_pipe(char *tab, t_all *all);
-int			ft_ispipe(char *tab);
-void		istabpipe(char *tab, t_all *all);
+int					ft_pipe(char *tab, t_all *all);
+int					ft_ispipe(char *tab);
+void				istabpipe(char *tab, t_all *all);
 
 /*
 ** --REDIRECTION--
 */
 
-char		*ft_redirection(char *tab, t_all *all);
+char				*ft_redirection(char *tab, t_all *all);
 
 /*
 ** --SPLITSLASH--
 */
 
-char		**ft_splitslash(const char *str, char *c);
-int			ischarset(const char *str, int is, char *c);
-int			isguillemet(int is, const char *str);
-char		**ft_splitspace(const char *str, char c);
-char		*ft_splitbuiltin(const char *str, char c);
+char				**ft_splitslash(const char *str, char *c);
+int					ischarset(const char *str, int is, char *c);
+int					isguillemet(int is, const char *str);
+char				**ft_splitspace(const char *str, char c);
+char				*ft_splitbuiltin(const char *str, char c);
 
 /*
 ** --EXECUTION--
 */
 
-int			ft_exec(t_all *all, char *tab);
-int			isexec(char *tab);
-char		**ft_allpath(t_all *all);
-char		*ft_exist(t_all *all, char *tab, int i);
-void		ft_freexec(char **tab);
-char		*ft_strtrimslash(char const *str, char const *set);
+int					ft_exec(t_all *all, char *tab);
+int					isexec(char *tab);
+char				**ft_allpath(t_all *all);
+char				*ft_exist(t_all *all, char *tab, int i);
+void				ft_freexec(char **tab);
+char				*ft_strtrimslash(char const *str, char const *set);
 
 #endif
