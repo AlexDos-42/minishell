54419c69fd4a8fe51f10daf268e130682a9c24d7
AlexDos-42 <92.alex.ds@gmail.com>
2020-09-24 15:19:53 +0200

    norme

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -12,14 +12,10 @@
 
 #include "../include/minishell.h"
 
-int		ft_exitret(char **tab)
+int		ft_exitret(char **tab, int i, int sign)
 {
 	unsigned long int res;
-	int		i;
-	int		sign;
 
-	i = 0;
-	sign = 1;
 	while (tab[0][i] == '\t' || tab[0][i] == '\f' || tab[0][i] == '\r'
 	|| tab[0][i] == ' ' || tab[0][i] == '\'')
 		i++;
@@ -29,12 +25,8 @@ int		ft_exitret(char **tab)
 	res = 0;
 	while (ft_isdigit(tab[0][i]))
 		res = res * 10 + tab[0][i++] - '0';
-	if (sign == 1 && res > (unsigned long int)LONG_MAX)
-	{
-		ft_printf("minishell: exit: %s: numeric argument required\n", tab[0]);
-		res = 2;
-	}
-	if (sign == -1 && res > (unsigned long int)LONG_MAX + 1)
+	if ((sign == 1 && res > (unsigned long int)LONG_MAX)
+		|| (sign == -1 && res > (unsigned long int)LONG_MAX + 1))
 	{
 		ft_printf("minishell: exit: %s: numeric argument required\n", tab[0]);
 		res = 2;
@@ -49,34 +41,20 @@ int		ft_exitret(char **tab)
 	return (g_ret = res);
 }
 
-int		ft_cleanexit(char *tab)
+int		isnumberexit(char **new, int i)
 {
-	int		i;
-	char	**new;
-	char	*tmp;
-
-	i = 0;
-	tmp = ft_strtrimslash(tab, " ");
-	free(tab);
-	if (!(tmp[0]))
-	{
-		free(tmp);
-		return (0);
-	}
-	new = ft_splitspace(tmp, ' ');
-	free(tmp);
-	new[0] = ft_suprguy(new[0]);
-	while (new[0][i] == ' ' || new[0][i] == '\t' || new[0][i] == '\f'
-	|| new[0][i] == '\r')
-		i++;
-	if (new[0][i] == '-' || new[0][i] == '+')
-		i++;
-	while (new[0][i])
-	{
-		if (new[0][i] != '1' && new[0][i] != '2' &&
+	if (new[0][i] != '1' && new[0][i] != '2' &&
 		new[0][i] != '3' && new[0][i] != '4' && new[0][i] != '5'
 		&& new[0][i] != '6' && new[0][i] != '7' &&
 		new[0][i] != '8' && new[0][i] != '9' && new[0][i] != '0')
+		return (1);
+	return (0);
+}
+
+int		ft_cleanexit(char **new, int i)
+{
+	while (new[0][i])
+		if (isnumberexit(new, i))
 		{
 			while (new[0][i] && ((new[0][i] == ' ' || new[0][i] == '\t') &&
 					(!new[0][i + 1] || new[0][i + 1] == ' '
@@ -92,18 +70,38 @@ int		ft_cleanexit(char *tab)
 		}
 		else
 			i++;
-	}
 	if (new[1])
 	{
 		ft_printf("minishell: exit: too many arguments\n");
 		ft_freexec(new);
 		return (1);
 	}
-	return (ft_exitret(new));
+	return (ft_exitret(new, 0, 1));
 }
 
 int		ft_exit(t_all *all)
 {
-	g_ret = ft_cleanexit(all->tab);
+	char	*tmp;
+	int		i;
+	char	**new;
+
+	i = 0;
+	tmp = ft_strtrimslash(all->tab, " ");
+	free(all->tab);
+	if (!(tmp[0]))
+	{
+		free(tmp);
+		g_ret = 0;
+		return (2);
+	}
+	new = ft_splitspace(tmp, ' ');
+	free(tmp);
+	new[0] = ft_suprguy(new[0]);
+	while (new[0][i] == ' ' || new[0][i] == '\t' || new[0][i] == '\f'
+	|| new[0][i] == '\r')
+		i++;
+	if (new[0][i] == '-' || new[0][i] == '+')
+		i++;
+	g_ret = ft_cleanexit(new, i);
 	return (2);
 }
