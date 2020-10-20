3c8070a3531a806fe5bacda64c6a8a5228af85db
Alex 42 <92.alex.ds@gmail.com>
2020-10-20 16:01:04 +0200

    projet validé

--- include/minishell.h
+++ include/minishell.h
@@ -31,35 +31,35 @@ int			g_pipe;
 typedef struct		s_all
 {
 	char			**argv;
-	int				fct;
+	int			fct;
 	char			*tab;
 	char			*pwd;
 	char			**env;
 	char			**ext;
-	int				nb_ext;
-	int				ret;
+	int			nb_ext;
+	int			ret;
 	size_t			nb_env;
-	int				fdin;
-	int				fdout;
-	int				fdinc;
-	int				fdoutc;
-}					t_all;
+	int			fdin;
+	int			fdout;
+	int			fdinc;
+	int			fdoutc;
+}				t_all;
 
 /*
 ** --MAIN--
 */
 
-int					main(int argc, char **argv, char **env);
+int				main(int argc, char **argv, char **env);
 void				ft_prompt(t_all *all, char *tmp, char *str, int i);
 
 /*
 ** --PARSING--
 */
 
-int					ft_minishell(t_all *all, char *str, int stop, int k);
-int					ft_nbfct(t_all *all, char *tab, int i, int jma);
-int					ft_ptrfct(t_all *all);
-int					ft_loop(char *tab, t_all *all);
+int				ft_minishell(t_all *all, char *str, int stop, int k);
+int				ft_nbfct(t_all *all, char *tab, int i, int jma);
+int				ft_ptrfct(t_all *all);
+int				ft_loop(char *tab, t_all *all);
 
 /*
 ** --INIT--
@@ -72,24 +72,24 @@ void				tab_env(char **env, t_all *all);
 ** --FONCTIONS--
 */
 
-int					ft_echo(t_all *all);
-int					ft_pwd(t_all *all);
-int					ft_cd(t_all *all);
-int					ft_exit(t_all *all);
-int					ft_env(t_all *all);
-int					ft_export(t_all *all);
+int				ft_echo(t_all *all);
+int				ft_pwd(t_all *all);
+int				ft_cd(t_all *all);
+int				ft_exit(t_all *all);
+int				ft_env(t_all *all);
+int				ft_export(t_all *all);
 char				**ft_freetab(char **tabnewenv, int i);
-int					isexporterror(char *tab, int j);
-int					ft_isenvexist(t_all *all, char *tab);
-int					isenvexist_ext(t_all *all, char *tab);
+int				isexporterror(char *tab, int j);
+int				ft_isenvexist(t_all *all, char *tab);
+int				isenvexist_ext(t_all *all, char *tab);
 void				put_export(int eg, t_all *all, int j, char *tab);
-int					ft_isenvexist_ext(t_all *all, char *tab);
+int				ft_isenvexist_ext(t_all *all, char *tab);
 char				**ft_exporterreur(char **str, int j);
-int					ft_unset(t_all *all);
+int				ft_unset(t_all *all);
 void				export_solo(t_all *all, int i, int eg, char *tmp);
-int					dble(char **tab, int i, int j, int k);
-int					dble_ext(char **tab, int i);
-int					iserrorput(char *tab, int j);
+int				dble(char **tab, int i, int j, int k);
+int				dble_ext(char **tab, int i);
+int				iserrorput(char *tab, int j);
 char				*new_tab(char *tab, int j);
 
 /*
@@ -100,16 +100,16 @@ char				*ft_replace(char *tab, t_all *all, int i, int j);
 char				*ft_isinenv(char *tab, t_all *all);
 char				*ft_newtab(char *tab, char *env, int i);
 void				ft_suprext(t_all *all, char *new, int p, int o);
-int					combien(char *new, int l, int *k);
+int				combien(char *new, int l, int *k);
 
 /*
 ** --PIPE--
 */
 
-int					ft_pipe(char *tab, t_all *all);
-int					ft_ispipe(char *tab);
+int				ft_pipe(char *tab, t_all *all);
+int				ft_ispipe(char *tab);
 void				istabpipe(char *tab, t_all *all);
-int					ft_ispipe(char *tab);
+int				ft_ispipe(char *tab);
 
 /*
 ** --REDIRECTION--
@@ -126,19 +126,19 @@ char				*join_tmp(char *new, int l, char *tab);
 */
 
 char				**ft_splitslash(const char *str, char *c);
-int					ischarset(const char *str, int is, char *c);
-int					isguillemet(int is, const char *str);
+int				ischarset(const char *str, int is, char *c);
+int				isguillemet(int is, const char *str);
 char				**ft_splitspace(const char *str, char c);
 char				*ft_splitbuiltin(const char *str, char c);
-int					isguillemet(int is, const char *str);
+int				isguillemet(int is, const char *str);
 char				*ft_suprguy(char *tab);
 
 /*
 ** --EXECUTION--
 */
 
-int					ft_exec(t_all *all, char *tab);
-int					isexec(char *tab);
+int				ft_exec(t_all *all, char *tab);
+int				isexec(char *tab);
 char				**ft_allpath(t_all *all);
 char				*ft_exist(t_all *all, char *tab, int i);
 void				ft_freexec(char **tab);
@@ -146,19 +146,19 @@ char				*ft_strtrimslash(char const *str, char const *set);
 void				istabpipe_suite3(char **new, t_all *all, int i);
 void				ft_create_file(t_all *all, char **tab, char **redir, int i);
 char				*ft_redirection(char *tab, t_all *all);
-int					redirspace(char **redir);
+int				redirspace(char **redir);
 char				*ft_join(char **redir, char **new, t_all *all);
-int					redirerror_bis(char **redir);
+int				redirerror_bis(char **redir);
 char				**ft_realsplit(char *tab, char **new, char **redir, int k);
 char				**ft_addarg(char **tmp);
 char				**ft_allredir(char *tab);
-int					redirerror(char **tab, char **redir);
+int				redirerror(char **tab, char **redir);
 char				**ft_allredir(char *tab);
 char				**ft_all_redir_bis(char *tab, char **new, int i, int j);
 char				*ft_ret(char *tab, int p);
-int					is_two_pipe(char *tab);
-int					ft_pipeinit(char *tab, t_all *all, int i, int p);
-int					is_two_pipe(char *tab);
+int				is_two_pipe(char *tab);
+int				ft_pipeinit(char *tab, t_all *all, int i, int p);
+int				is_two_pipe(char *tab);
 char				*if_or_else(char *new, char *tab, int l, int k);
 void				ft_zero(t_all *all);
 
